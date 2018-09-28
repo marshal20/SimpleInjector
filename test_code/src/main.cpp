4a3b02486859b028eb1e9d2c0438000ac9f5c59d
marshal20 <x.madkour@gmail.com>
2018-09-28 09:35:31 +0200

    created a simple thread load library function

--- test_code/src/main.cpp
+++ test_code/src/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <Windows.h>
+
+/*
+DWORD WINAPI ThreadProc(
+_In_ LPVOID lpParameter
+);
+*/
+
+const char* lib_path;
+
+DWORD WINAPI LoadDll(LPVOID pram)
+{
+	LoadLibraryA(lib_path);
+
+	return EXIT_SUCCESS;
+}
 
 int main()
 {
-	std::cout << "Hello World..." << std::endl;
-	std::cin.get();
+	HANDLE thread_handle;
+	lib_path = "C:\\Users\\xmadk\\Documents\\Visual Studio 2017\\Projects\\test_window_proc_thief\\bin\\Win32\\Debug\\window_proc_thief.dll";
+	thread_handle = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)LoadDll, NULL, NULL, NULL);
+	WaitForSingleObject(thread_handle, INFINITE);
+	Sleep(10000);
 
 	return 0;
 }
