3c21202686ac61dec6e0aaced415c53b686c6d75
marshal20 <x.madkour@gmail.com>
2018-11-30 17:03:37 +0200

    Finished the SimpleInjectorCmd

--- SimpleInjectorCmd/Program.cs
+++ SimpleInjectorCmd/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InjectorAPI;
 
 namespace SimpleInjectorCmd
 {
@@ -24,13 +25,14 @@ namespace SimpleInjectorCmd
             String DllPath = args[2];
             uint ProcId = 0u;
 
-            if(args[0] == "-p")
+            // Parse the process ID.
+            if(args[0] == "-p") // Name
             {
-                // Name
+                Backend.GetProcInfo(args[1]);
             }
-            else if(args[0] == "-pid")
+            else if(args[0] == "-pid") // ID
             {
-                // ID
+                ProcId = uint.Parse(args[1]);
             }
             else
             {
@@ -38,6 +40,16 @@ namespace SimpleInjectorCmd
                 return;
             }
 
+            // Check if the process ID is valid.
+            if(Backend.GetProcInfo(ProcId).id == 0)
+            {
+                Console.WriteLine("Invalid process.");
+            }
+
+            // Finally we inject the dll into the process.
+            Backend.InjectDll(ProcId, DllPath);
+
+            Console.Write("The dll was successfully injected into {0} process.", ProcId);
         }
     }
 }
