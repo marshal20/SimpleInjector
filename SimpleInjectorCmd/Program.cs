3082ccbef664ca41ee40d29e11f21bc2d1a54925
marshal20 <x.madkour@gmail.com>
2018-11-30 16:54:11 +0200

    Created the main body of SimpleInjectorCmd

--- SimpleInjectorCmd/Program.cs
+++ SimpleInjectorCmd/Program.cs
@@ -8,8 +8,35 @@ namespace SimpleInjectorCmd
 {
     class Program
     {
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage: SimpleInjectorCmd.exe <-p/-pid> <process_name/process_id> dll_path");
+        }
+
         static void Main(string[] args)
         {
+            if(args.Length != 3)
+            {
+                PrintUsage();
+                return;
+            }
+
+            String DllPath = args[2];
+            uint ProcId = 0u;
+
+            if(args[0] == "-p")
+            {
+                // Name
+            }
+            else if(args[0] == "-pid")
+            {
+                // ID
+            }
+            else
+            {
+                PrintUsage();
+                return;
+            }
 
         }
     }
